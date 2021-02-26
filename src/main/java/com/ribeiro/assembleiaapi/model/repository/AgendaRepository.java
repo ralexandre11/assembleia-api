17eb9e8b75c170b894dfd3c25af1ee0710b3e339
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-26 17:34:05 -0300

    Add repository model

--- src/main/java/com/ribeiro/assembleiaapi/model/repository/AgendaRepository.java
+++ src/main/java/com/ribeiro/assembleiaapi/model/repository/AgendaRepository.java
@@ -1,8 +1,9 @@
 package com.ribeiro.assembleiaapi.model.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.ribeiro.assembleiaapi.model.entity.Agenda;
 
-public class AgendaRepository extends JpaRepository<Agenda, Long> {
-	
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
 
 }
