4e2839ef8842ec02bae02543b6cda9f931e5f3db
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-02-28 10:08:39 -0300

    Created end point to open session Agenda

--- src/main/java/com/ribeiro/assembleiaapi/resource/AgendaController.java
+++ src/main/java/com/ribeiro/assembleiaapi/resource/AgendaController.java
@@ -1,8 +1,13 @@
 package com.ribeiro.assembleiaapi.resource;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,28 +21,29 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/agenda")
 @RequiredArgsConstructor
 public class AgendaController {
-	
+
 	private final AgendaServiceImpl service;
-	
-//	@GetMapping
-//	public ResponseEntity<List<AgendaDTO>> all() {
-//		return ResponseEntity.status(HttpStatus.OK).body(service.all());
-//	}
+
+	@GetMapping
+	public ResponseEntity<List<AgendaDTO>> getAll() {
+		return ResponseEntity.status(HttpStatus.OK).body(service.getAll());
+	}
 
 //	@PostMapping
 //	public ResponseEntity<AgendaDTO> createDiscipline(@RequestBody Discipline discipline) {
 //		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(discipline));
 //	}
-	
+
 	@PostMapping
-	public ResponseEntity<AgendaDTO> saveAgenda(@RequestBody AgendaDTO dto ) {
-//		try {
-			AgendaDTO dtoSaved = service.save(dto);
-			return ResponseEntity.status(HttpStatus.CREATED).body(dtoSaved);
-//		} catch (Exception e) {
-//			return ResponseEntity.badRequest().body(e.getMessage());
-//		}
+	public ResponseEntity<String> createAgenda(@RequestBody AgendaDTO dto) {
+		service.save(dto);
+		return ResponseEntity.status(HttpStatus.CREATED).body("Created Agenda!");
 	}
 
-	
+	@PutMapping("/{id}")
+	public ResponseEntity<AgendaDTO> openSessionAgenda(@PathVariable("id") Long id, @RequestBody AgendaDTO dto) {
+		AgendaDTO dtoSaved = service.update(id, dto);
+		return ResponseEntity.status(HttpStatus.OK).body(dtoSaved);
+	}
+
 }
