e78b54a89e855a3ae980266ef314ffb4087b4036
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-03-01 11:02:57 -0300

    Config swagger documetation

--- src/main/java/com/ribeiro/assembleiaapi/AssembleiaApiApplication.java
+++ src/main/java/com/ribeiro/assembleiaapi/AssembleiaApiApplication.java
@@ -1,7 +1,12 @@
 package com.ribeiro.assembleiaapi;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class AssembleiaApiApplication {
@@ -10,4 +15,15 @@ public class AssembleiaApiApplication {
 		SpringApplication.run(AssembleiaApiApplication.class, args);
 	}
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+     return new OpenAPI()
+          .info(new Info()
+          .title("Assembleia API")
+         .version(appVersion)
+          .description(appDesciption)          
+          //.termsOfService("http://swagger.io/terms/")
+          //.license(new License().name("Apache 2.0").url("http://springdoc.org"))
+          );
+    }
 }
