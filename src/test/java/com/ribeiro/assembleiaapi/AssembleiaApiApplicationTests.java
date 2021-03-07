5861b57ea95df67a1217acf1a555b9cd9521e2d0
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-03-06 20:16:05 -0500

    Add create agenda integration test

--- src/test/java/com/ribeiro/assembleiaapi/AssembleiaApiApplicationTests.java
+++ src/test/java/com/ribeiro/assembleiaapi/AssembleiaApiApplicationTests.java
@@ -1,13 +1,52 @@
 package com.ribeiro.assembleiaapi;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import com.ribeiro.assembleiaapi.model.dto.AgendaAddDTO;
+import com.ribeiro.assembleiaapi.resource.dto.ResponseDTO;
+
+@SpringBootTest(classes = AssembleiaApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class AssembleiaApiApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void givenDescription_whenPostAgenda_thenStatusCreated() {
+		HttpEntity<AgendaAddDTO> request = createRequest();
+
+		ResponseEntity<ResponseDTO> response = create(request);
+
+		Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+	}
+
+	private ResponseEntity<ResponseDTO> create(final HttpEntity<AgendaAddDTO> request) {
+		return restTemplate.postForEntity(getUrl(), request, ResponseDTO.class);
+	}
+
+	private HttpEntity<AgendaAddDTO> createRequest() {
+		return new HttpEntity<AgendaAddDTO>(createPayload());
+	}
+
+	private AgendaAddDTO createPayload() {
+		return new AgendaAddDTO("my description");
+	}
+
+	private String getUrl() {
+		return "http://localhost:" + this.port + "/agenda";
 	}
 
 }
