40197c798f3c7937ce00bf40ba89f0b58e6086a9
Ricardo Ribeiro <ralexandre11.gmail.com>
2021-03-06 20:09:37 -0500

    Add agenda repo test

---
+++ src/test/java/com/ribeiro/assembleiaapi/model/repository/AgendaRepositoryIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.ribeiro.assembleiaapi.model.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.ribeiro.assembleiaapi.model.entity.Agenda;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class AgendaRepositoryIntegrationTest {
+
+	@Autowired
+	AgendaRepository repository;
+
+	@Autowired
+	TestEntityManager testEntityManager;
+
+	@Test
+	void givenAgenda_whenSave_thenAgendaIsSaved() {
+		Agenda agenda = new Agenda();
+		agenda.setDescription("my description");
+
+		Agenda agendaSaved = repository.save(agenda);
+
+		Assertions.assertThat(agendaSaved.getId()).isNotNull();
+		Assertions.assertThat(agendaSaved.getDescription()).isEqualTo("my description");
+		Assertions.assertThat(agendaSaved.getExpiration()).isNull();
+		Assertions.assertThat(agendaSaved.getVotes()).hasSize(0);
+	}
+}
